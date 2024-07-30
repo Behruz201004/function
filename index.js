9aaf7e9b5be4f228fe881c2de38d7aeb957147d6
Behruz201004 <behruzbahtiyorov888@gmail.com>
2024-07-30 13:50:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+// #1
+// for (let i = 0; i < 10; i++) {
+//     console.log("hello");
+    
+// }
+
+// #2
+// let i = 0
+// while(i < 5 ){
+//  console.log(i);
+//  i++
+// }
+
+// #3
+// let i = 0
+// do{
+//    i++ 
+//   console.log(i);
+  
+// }while(i > 10)
+
+
+
+// function sayHello(name) {
+// console.log('salom ' +  name );
+// }
+
+// sayHello("kvantum2")
+
+
+// function sum( a, b) {
+//     console.log(a + b);
+// }
+
+// sum(50 , 10)
+
+// sum(1,2)
+
+// sum(10 , 1)
+
+// sum( 'kvantum ',     'Valiev'  )
+
+// greet('Beha')
+// expression ()
+// const greet = function(a) {
+//     console.log("qalesan " + "a" ) ;
+// }
+
+// greet( "Eldor ")
+// greet('Otabek')
+
+
+
+
+// const плюс = ( a , b) => a + b  
+// const минус = ( a , b) => a - b  
+// const умножение = ( a , b) => a * b  
+// const деление  = ( a , b) => a / b  
+
+// console.log(плюс(5 + 5));
+// console.log(минус(5 - 5));
+// console.log(умножение(200 * 5));
+// console.log(деление(1000 / 100));
+
+
+
+
+
+
+// const me = function(name , surname , age ) {
+//     console.log( 'Пртвет меня завуд ' +  name + 'Beha'+ surname + ', мне ' + age + 'лет' ) ;
+// }
+
+
+
+
+const me = function(name , surname , age ) {
+   console.log(`Пртвет меня завуд  ${name}  ${surname} , мне  ${age} лет `     );
+}
+me('Beha ', 'Behovich' , 13  )
+
+
